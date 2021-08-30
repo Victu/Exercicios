dd1a5820bfc801b38561bea538ebf352368bfe84
Victor Silva <victorsilva.lion@gmail.com>
2021-08-30 19:45:24 -0300

    Pequenas correções

--- Resolução/Fibonacci.py
+++ Resolução/Fibonacci.py
@@ -8,25 +8,23 @@ st = 1  #st -> Segundo termo
 repetir = ""
 sair = False
 
-print('----' * Tt)
+print('-----' * Tt)
 while x <= Tt and not sair:
     pt = st
     st = pt_st
     pt_st = pt + st  #pt_st -> Soma do primeiro termo com o segundo termo
-    print(st, end=", " if x < Tt else '...')
+    print(st, end=", " if x < Tt - 1 else '...\n')
     x += 1
 
     if x == Tt:
-        print('')
-        print('----' * Tt)
-        repetir = input('\nDeseja repetir? ')[0]
+        print('-----' * Tt)
+        repetir = input('\nDeseja repetir[sim/não]? ').strip()[0]
         if repetir in 'Nn':
             sair = True
-        else:
+        elif repetir in 'Ss':
             Tt = int(input('Informe quantos termos quer mostrar: '))
-            print('----' * Tt)
+            print('-----' * Tt)
             pt = pt_st = x = 0
             st = 1
-
 print('-' * 66)
 print('{:^60}'.format(' SEQUÊNCIA FINALIZADA.'))
